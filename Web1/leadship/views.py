e45c1cd0ae53facf02b7cadc8d77f62562937dcb
SeoJB95 <seo.jb95@gmail.com>
2021-02-24 12:11:19 +0900

    hull view page 복구

--- Web1/leadship/views.py
+++ Web1/leadship/views.py
@@ -6,14 +6,13 @@ import sys
 import pprint
 #from HGB_project import *
 # Create your views here.
-'''
 def test_hullform(request):
     with open('/workspace/data/DS2500TEU_2.proj','rb') as f:
         project = pickle.load(f)
     Hull = project.hull['DS2500TEU_forPaper']
     json = Hull.ToJson()
     return render(request, 'importHull.html',{'testjson':json})
-'''
+
 def create(request):
     ships = ShipMainParticular.objects.order_by('ship_size')
 
